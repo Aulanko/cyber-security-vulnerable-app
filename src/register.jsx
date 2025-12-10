a4f998858c4b4f4f3a44135a269524a76b0071b8
pronom8 <mangros19@gmail.com>
2025-12-10 12:15:16 +0200

    simple faulty registration and saving to csv file made

--- src/register.jsx
+++ src/register.jsx
@@ -1,20 +1,30 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 function Register() {
     const [username,setUsername]=useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate()
 
 
-    const handleSubmit = async(e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log("username;", username)
-        console.log("rmail", email)
-        console.log("paswrod", password)
-        console.log(e.target.value)
-
+        
+     
+        fetch("http://localhost:3001/save-user", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, email, password })
+        }).then(()=>{
+          navigate("/")
+        })
+        
+      
+      
+        
+        alert("Data sent")
     }
 
     
--- src/server.js
+++ src/server.js
@@ -1,25 +1,28 @@
 const express = require('express')
 const app = express()
+const fs = require("fs")
 const PORT = 3001
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173') 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Origin', '*') 
+
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
     next()
 })
 
 app.use(express.json())
 
-
-app.get('/', (req, res) => {
-    res.send('Server is working!')
-})
-
-
 app.post('/save-user', (req, res) => {
     console.log('Received user:', req.body)
-    res.json({ message: 'User received!', data: req.body })
+    
+    const user = req.body
+    const values = Object.values(user).join(',') + '\n'
+    
+ 
+    fs.appendFile("users.csv", user, () => {
+        res.json({ message: "Saved!" })
+    })
+    app.route("/")
 })
 
 app.listen(PORT, () => {
